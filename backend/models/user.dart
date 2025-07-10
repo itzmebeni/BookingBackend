23297846773424a83fcbc9354d79da7b0cb604a3
ryzenmagsino <magsino301107@gmail.com>
2025-07-10 16:41:46 +0800

    W3-D3 login/signup

--- backend/models/user.dart
+++
@@ -1,9 +0,0 @@
-class User {
-  final String username;
-
-  User({required this.username});
-
-  factory User.fromJson(Map<String, dynamic> json) {
-    return User(username: json['username']);
-  }
-}
